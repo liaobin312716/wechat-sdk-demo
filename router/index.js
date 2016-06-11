577e2342efbf5b4024d5da9fa333265a45ef2089
liaobin_iwm <liaobin@iwaimai.baidu.com>
2016-06-11 13:36:28 +0800

    update

--- router/index.js
+++ router/index.js
@@ -8,17 +8,8 @@ exports.init = function (app) {
 
 	
 	app.get('/',function(req,res){
+		//var url = req.protocol + '://' + req.host + req.path;
 		var url = req.protocol + '://' + req.host + req.originalUrl; //获取当前url
-		signature.sign(url,function(signatureMap){
-			signatureMap.appId = wechat_cfg.appid;
-			res.render('index',signatureMap);
-		});
-	});
-
-	app.get('/wx', function(req,res){
-		var url = req.protocol + '://' + req.host + req.originalUrl; //获取当前url
-		console.log('url ======########======= ', url);
-		var url = req.protocol + '://' + req.host + req.originalUrl; //获取当前url 包含?后的但不包含#号后的
 		signature.sign(url,function(signatureMap){
 			signatureMap.appId = wechat_cfg.appid;
 			res.render('index',signatureMap);
